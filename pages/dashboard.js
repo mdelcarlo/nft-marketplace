628ae8e7c054873c75a0c389e576e49879db8b87
Matias Del Carlo <matias.delcarlo@dexibit.com>
2022-03-10 10:20:19 -0300

    Add dashboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,99 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftaddress, nftmarketaddress } from "../config";
+
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import NFTMarketplace from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+
+export default function Dashboard() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      NFTMarketplace.abi,
+      signer
+    );
+    const data = await marketContract.fetchItemsCreated();
+
+    const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await nftContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          sold: i.sold,
+          image: meta.data.image,
+        };
+        return item;
+      })
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>;
+  const soldItems = nfts.filter((i) => i.sold);
+  return (
+    <div>
+      <div className="p-4">
+        <h2 className="text-2xl py-2">Items Listed</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <div className="p-4 h-96">
+                <img src={nft.image} className="m-auto" />
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} Matic
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="px-4">
+        {Boolean(soldItems.length) && (
+          <div>
+            <h2 className="text-2xl py-2">Items sold</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {soldItems.map((nft, i) => (
+                <div
+                  key={i}
+                  className="border shadow rounded-xl overflow-hidden"
+                >
+                  <div className="p-4 h-96">
+                    <img src={nft.image} className="m-auto" />
+                  </div>{" "}
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">
+                      Price - {nft.price} Matic
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
